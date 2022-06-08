c606b5cc9561e2a2dac08864a70e4008f47f2930
Geovanny Jaramillo <byronjaramillo_hotmail.com>
2022-06-08 12:55:30 -0500

    Finalizado Conversion 2 unidades

--- conversor.h
+++ conversor.h
@@ -14,7 +14,9 @@ class Conversor : public QDialog
 public:
     Conversor(QWidget *parent = nullptr);
     ~Conversor();
-
+public slots:
+    void cent2fahr(int grados); //de centigrados a fare (centigrados 2=to fareh)
+    void fareh2cent(int grados); // de fareh a centigrados
 private:
     Ui::Conversor *ui;
 };
